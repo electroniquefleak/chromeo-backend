d9449e44ebf6c33ef746eb9a0c7224dd9e5959ec
Hunter <fleakhunter@gmail.com>
2022-01-03 12:40:48 -0500

    Set Track params and add corresponding CRUD methods for Track Controller.

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,2 +1,23 @@
 class TracksController < ApplicationController
+
+    def show
+        @track = Track.find(params[:id])
+        render json: @track.as_json
+    end
+
+    def create
+        @track = Track.new(track_params)
+        if @track.save
+            puts "Track added!"
+            render json: @track
+        else
+            puts "Error: Failed to render track."
+        end
+    end
+
+    private
+
+    def track_params
+        params.require(:track).permit(:spotify_track_id)
+    end
 end
