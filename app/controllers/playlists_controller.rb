5fb6abe5bf35f67c60a6bc17e6b9e20a5c25368c
Hunter <fleakhunter@gmail.com>
2022-01-03 12:50:31 -0500

    Set Playlist params and add corresponding CRUD methods for Playlist Controller.

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,2 +1,30 @@
 class PlaylistsController < ApplicationController
+    def index
+        @playlists = Playlist.all
+        render json: @playlists
+    end
+
+    def show
+        @playlist = Playlist.find(params[:id])
+        render json: @playlist.as_json
+    end
+
+    def create
+        @playlist = Playlist.new(playlist_params)
+        if @playlist.save
+            puts "Playlist created!"
+            render json: @playlist
+        else
+            puts "Error: Failed to compile playlist."
+        end
+    end
+
+    private
+
+    def playlist_params
+        params.require(:playlist).permit(
+            :name,
+            :playlist_id,
+            :spotify_playlist_id)
+    end
 end
